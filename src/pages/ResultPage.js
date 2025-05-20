5930163ad826183115e570024357ad8243af5338
yuan1211 <j45979531@gmail.com>
2025-05-20 10:38:26 +0900

    결과 화면 이미지 추가

--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import image1 from '../happy.png';
+import image2 from '../Sad.png';
 
 
 function ResultPage({ score, setScore }) {
@@ -11,12 +13,16 @@ function ResultPage({ score, setScore }) {
 
     };
 
-    const endingMent = score >= 3 ? '자네는 졸업일세!' : '졸업은 꿈도 꾸지 말게!!'
+    const endingMent = score >= 3 ? '자네는 졸업일세!' : '졸업은 꿈도 꾸지 말게!!';
+    const resultImage = score >= 3 ? image1 : image2;
 
     return (
         <div className="container">
             <h1>결과</h1>
             <p>총 5문제 중 {score}개 맞았어요!</p>
+
+            <img src={resultImage} alt="결과 이미지" style={{width: '250px', marginTop: '5px'}}/>
+
             <h2>{endingMent}</h2>
 
             <button className="next-button" onClick={goToStart}>
@@ -26,4 +32,4 @@ function ResultPage({ score, setScore }) {
     );
 }
 
-export default ResultPage;
+    export default ResultPage;
